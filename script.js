97f67e0bc326fd0a810ce30605d6660fb4e19b55
Sandra <oyarekhuas@gmail.com>
2022-10-18 18:36:05 +0100

    Done

--- script.js
+++ script.js
@@ -1,53 +1,15 @@
- function openNav() {
-                document.getElementById("myNav").style.width = "70%";
-            }
 
-            function closeNav() {
-                document.getElementById("myNav").style.width = "0%";
-            }
+const toggle = document.getElementById('toggle')
+const sidebar = document.getElementById('sidebar')
 
-            var menuA = document.getElementById("monk1");
-                menuA.addEventListener("click", function () {
-                    closeNav()
-                });
+document.body.onclick = function(e) {
+    if (e.target.id !== 'sidebar' && e.target.id !== 'toggle') {
+        toggle.classList.remove('active')
+        sidebar.classList.remove('active')
+    }
+}
 
-                var menuB = document.getElementById("monk2");
-                menuB.addEventListener("click", function () {
-                    closeNav()
-                });
-
-                var menuC = document.getElementById("monk3");
-                menuC.addEventListener("click", function () {
-                    closeNav()
-                });
-
-                var menuD = document.getElementById("monk4");
-                menuD.addEventListener("click", function () {
-                    closeNav()
-                });
-
-                var menuE = document.getElementById("monk5");
-                menuE.addEventListener("click", function() {
-                    closeNav()
-                });
-
-/*
-const boxes = document.querySelectorAll('.boxa')
-
-window.addEventListener('scroll', checkBoxes)
-
-checkBoxes()
-
-function checkBoxes() {
-    const triggerBottom = window.innerHeight / 3 * 4
-
-    boxes.forEach(boxa => {
-        const boxTop = boxa.getBoundingClientRect().top
-
-        if(boxTop < triggerBottom) {
-            boxa.classList.add('show')
-        } else {
-            boxa.classList.remove('show')
-        }
-    })
-}*/
+toggle.addEventListener('click', function() {
+    toggle.classList.toggle('active')
+    sidebar.classList.toggle('active')
+})
